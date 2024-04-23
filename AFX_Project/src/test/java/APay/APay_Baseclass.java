ecd3e2de3c0cc376265c8c39203e159c7e0a2552
Admin <Admin@DESKTOP-M1H2EC0>
2024-04-23 16:56:10 +0530

    commit

--- AFX_Project/src/test/java/APay/APay_Baseclass.java
+++ AFX_Project/src/test/java/APay/APay_Baseclass.java
@@ -43,33 +43,4 @@ public class APay_Baseclass {
 		wait = new WebDriverWait(driver, 10); // Initialize the wait object with a timeout of 10 seconds.
 	}
 	
-	
-//	public static void excelreader(Apay_Login_POM loginPage) throws InvalidFormatException {
-//	 try {
-//         // Load the Excel file
-//         FileInputStream file = new FileInputStream("F:\\AFX Login creds\\AFX_Login_creds.xlsx");
-//         Workbook workbook = WorkbookFactory.create(file);
-//         
-//         // Get the first sheet
-//         Sheet sheet = workbook.getSheetAt(0);
-//         
-//         // Iterate through each row
-//         for (Row row : sheet) {
-//             // Assuming the columns are in order: customerID, username, password
-//             String customerID = row.getCell(0).getStringCellValue();
-//             String username = row.getCell(1).getStringCellValue();
-//             String password = row.getCell(2).getStringCellValue();
-//             
-//             loginPage.entercredentials(customerID, username, password);
-//             
-//             // Do something with the data (print it for now)
-//             System.out.println("Customer ID: " + customerID + ", Username: " + username + ", Password: " + password);
-//         }
-//         
-//         // Close the file
-//         file.close();
-//     } catch (IOException e) {
-//         e.printStackTrace();
-//     }
-// }
 }

fbb8b1625159c6b3cbf0fbf4c27fcee7325677a3
Admin <Admin@DESKTOP-M1H2EC0>
2024-05-14 11:39:51 +0530

    Re parallel commit

--- AFX_Project/src/test/java/APay/Apay_Runner.java
+++ AFX_Project/src/test/java/APay/Apay_Runner.java
@@ -6,7 +6,7 @@ import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/resources/FeatureFiles/APay_Login.feature", glue = "APay", dryRun = false, monochrome = true,
+@CucumberOptions(features = "src/test/resources/FeatureFiles/", glue = "APay", dryRun = false, monochrome = true,
 				plugin = {"pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
 				)
 
